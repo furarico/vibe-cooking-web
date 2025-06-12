b42a828fe4ed9c5c70a64abdcf870a5ee14b7f49
Kanta Oikawa <iam@kantacky.com>
2025-06-12 15:48:34 +0900

    Update Firebase Admin

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,4 +1,25 @@
+import { credential } from 'firebase-admin';
 import { getApp, getApps, initializeApp } from 'firebase-admin/app';
+
+const getFirebaseAdminApp = () => {
+  if (getApps().length > 0) {
+    return getApp();
+  }
+
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    return initializeApp();
+  }
+
+  const config = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  };
+
+  return initializeApp({
+    credential: credential.cert(config),
+  });
+};
 
 const app = getApps().length === 0 ? initializeApp() : getApp();
 
