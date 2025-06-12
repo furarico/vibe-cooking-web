fd2e728512c76fd02ad19006c40c5655e01f632d
Kanta Oikawa <iam@kantacky.com>
2025-06-12 13:42:55 +0900

    [wip] Update db

---
+++ src/lib/database-connector.ts
@@ -0,0 +1,15 @@
+import { Connector, IpAddressTypes, AuthTypes } from '@google-cloud/cloud-sql-connector';
+
+export async function getClientOptions() {
+  const connector = new Connector();
+
+  const instanceConnectionName = `${process.env.CLOUD_SQL_PROJECT_ID}:${process.env.CLOUD_SQL_REGION}:${process.env.CLOUD_SQL_INSTANCE_NAME}`;
+
+  const clientOpts = await connector.getOptions({
+    authType: AuthTypes.IAM,
+    ipType: IpAddressTypes.PUBLIC,
+    instanceConnectionName,
+  });
+
+  return clientOpts;
+}
