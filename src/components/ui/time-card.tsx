0524c94983c065e35080884209e95f493a0920d9
Ayanentity <153336210+ayanentity@users.noreply.github.com>
2025-06-13 20:39:51 +0900

    time-cardを追加 (#44)
    * time-cardを追加
    
    * time-cardを追加
    
    * time-card componentを修正
    
    * Fix lint error

---
+++ src/components/ui/time-card.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+import { Card } from './card';
+
+interface TimeCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+  label?: string;
+}
+
+const TimeCard = React.forwardRef<HTMLDivElement, TimeCardProps>(
+  ({ className, title, label, ...props }, ref) => {
+    return (
+      <Card
+        className={cn(
+          'w-full items-center flex gap-0 rounded-md border border-slate-200 bg-white px-4 py-2 shadow-md transition-all hover:shadow-lg cursor-pointer flex-col',
+          className
+        )}
+        ref={ref}
+        {...props}
+      >
+        {/* タイトルを表示 */}
+        <p className="text-m font-Medium text-slate-500">{title ?? ''}</p>
+        {/* タイトルを表示 */}
+        <p className="text-lg font-semibold text-slate-900">{label ?? ''}</p>
+      </Card>
+    );
+  }
+);
+
+export type { TimeCardProps };
+TimeCard.displayName = 'TimeCard';
+
+export { TimeCard };
