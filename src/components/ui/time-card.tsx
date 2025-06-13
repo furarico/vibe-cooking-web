b71ead067bd4620c0ed79355cbba8bdc921da7a1
ayanentity <chikinnanman14@gmail.com>
2025-06-13 20:33:19 +0900

    time-card componentを修正

--- src/components/ui/time-card.tsx
+++ src/components/ui/time-card.tsx
@@ -1,9 +1,6 @@
-import { Timer } from 'lucide-react';
-import Image from 'next/image';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
-import { cva } from 'class-variance-authority';
 import { Card } from './card';
 
 interface TimeCardProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -12,26 +9,19 @@ interface TimeCardProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const TimeCard = React.forwardRef<HTMLDivElement, TimeCardProps>(
-  (
-    {
-      className,
-      title,
-      label,
-      ...props
-    },
-    ref
-  ) => {
+  ({ className, title, label, ...props }, ref) => {
     return (
       <Card
-        className={cn("w-full items-center flex gap-0 rounded-md border border-slate-200 bg-white px-4 py-2 shadow-md transition-all hover:shadow-lg cursor-pointer flex-col")}
+        className={cn(
+          'w-full items-center flex gap-0 rounded-md border border-slate-200 bg-white px-4 py-2 shadow-md transition-all hover:shadow-lg cursor-pointer flex-col'
+        )}
         ref={ref}
         {...props}
       >
-         {/* タイトルを表示 */}
-        {title && <p className="text-m font-Medium text-slate-500">{title}</p>}
         {/* タイトルを表示 */}
-        {label && <p className="text-lg font-semibold text-slate-900">{label}</p>}
-
+        <p className="text-m font-Medium text-slate-500">{title ?? ''}</p>
+        {/* タイトルを表示 */}
+        <p className="text-lg font-semibold text-slate-900">{label ?? ''}</p>
       </Card>
     );
   }
