e5f32bd1db62bc1722daada2363051dee1d56f31
Kanta Oikawa <iam@kantacky.com>
2025-06-16 14:35:02 +0900

    Update UI

--- src/components/ui/fixed-bottom-button.tsx
+++ src/components/ui/fixed-bottom-button.tsx
@@ -9,7 +9,7 @@ interface FixedBottomButtonProps {
 export function FixedBottomButton({ href, children }: FixedBottomButtonProps) {
   return (
     <div className="w-full fixed bottom-0 left-0 right-0 bg-white border-t border-slate-200">
-      <div className="max-w-xl mx-auto px-2 pt-4 pb-8 lg:pb-4">
+      <div className="max-w-xl mx-auto px-4 pt-4 pb-8 lg:pb-4">
         <Button asChild className="w-full">
           <Link href={href}>{children}</Link>
         </Button>
