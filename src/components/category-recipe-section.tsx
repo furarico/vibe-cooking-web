bb0f78b624388965e5ac918c8aaa4aacf5c91836
Hikaru Saito <beretta0602@gmail.com>
2025-06-18 15:09:52 +0900

    ボタンリンク

--- src/components/category-recipe-section.tsx
+++ src/components/category-recipe-section.tsx
@@ -4,6 +4,7 @@ import { RecipeCardList } from '@/components/ui/recipe-card-list';
 import { Category } from '@/lib/api-client';
 import Link from 'next/link';
 import { Suspense } from 'react';
+import { Button } from './ui/button';
 
 export function CategoryRecipeSection({ category }: { category: Category }) {
   const { recipes, loading } = useRecipesByCategoryPresenter(category.id);
@@ -16,9 +17,9 @@ export function CategoryRecipeSection({ category }: { category: Category }) {
     <Suspense fallback={<Loading />}>
       <div className="mt-8 mb-4 flex flex-row justify-between items-center">
         <h2 className="text-2xl font-bold text-gray-900">{category.name}</h2>
-        <Link href={`/recipes?category=${category.name}`}>
-          <span className="text-md text-slate-600 underline">もっと見る</span>
-        </Link>
+        <Button variant="link" asChild>
+          <Link href={`/recipes?category=${category.name}`}>もっと見る</Link>
+        </Button>
       </div>
       {loading ? <Loading /> : <RecipeCardList recipes={recipes} />}
     </Suspense>
