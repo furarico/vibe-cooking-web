bd45ab819a2d8959f3447feb4508cfad3b7c8986
Kanta Oikawa <iam@kantacky.com>
2025-06-20 01:27:27 +0900

    Add apple-app-site-association

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  headers: async () => {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [{ key: 'content-type', value: 'application/json' }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
